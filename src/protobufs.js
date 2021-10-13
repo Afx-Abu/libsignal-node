f83125f77bfc530e42987eb6351be63a13d998a9
Adhiraj Singh <adhirajsingh1001@gmail.com>
2021-10-13 17:10:33 +0530

    pregenerated protobuf

--- src/protobufs.js
+++ src/protobufs.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const path = require('path');
-const protobuf = require('protobufjs');
+const {
+    textsecure: {
+        WhisperMessage,
+        PreKeyWhisperMessage
+    }
+} = require('./WhisperTextProtocol.js');
 
-const protodir = path.resolve(__dirname, '../protos/');
-const p = protobuf.loadSync(path.join(protodir, 'WhisperTextProtocol.proto')).lookup('textsecure');
-
-module.exports = {
-    WhisperMessage: p.lookup('WhisperMessage'),
-    PreKeyWhisperMessage: p.lookup('PreKeyWhisperMessage')
-};
+module.exports = { WhisperMessage, PreKeyWhisperMessage };
